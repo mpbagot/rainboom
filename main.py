0743daeaad9241ae0b5bcf1e4401da67c9155f6c
Mitchell Bagot <packof14eggies@gmail.com>
2017-12-30 17:48:53 +1100

    Clean Up Code And Add FPS Counter

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 import math
-from time import sleep
+from time import sleep, time
 
 #Initialise the constants
 SCREEN_SIZE = (1200, 675)
+# SCREEN_SIZE = (800, 450)
 # the smaller this is, the bigger the fov (???)
 CAMERA_DEPTH = 750
 RENDER_DISTANCE = 20
@@ -17,19 +18,27 @@ class Camera:
     def __init__(self, pos, rot):
         self.rot = rot
         self.pos = pos
+        self.fps = 0
 
     def renderPoints(self, points):
+        start = time()
+        # points2 = [getLocalPos(point, self) for point in points]
+        # print(points2)
         for point in points:
             localPos = getLocalPos(point, self)
+            if localPos[2] < 0:
+                continue
             dist = getDistance(point, self)
             scale = int((1-(dist/RENDER_DISTANCE))*10) if dist < RENDER_DISTANCE else 0
 
             screenPos = projectPoint(localPos)
             try:
-                pygame.draw.circle(screen, (0, 0, 0), screenPos, scale)
+                if 0 < screenPos[0] < SCREEN_SIZE[0] and 0 < screenPos[1] < SCREEN_SIZE[1]:
+                    pygame.draw.circle(screen, (0, 0, 0), screenPos, scale)
             except OverflowError:
                 print('Scale:', scale)
                 print('Screen Position:', [round(a, 2) for a in screenPos])
+        self.fps = 1/(time()-start)
 
     def renderDebug(self):
         rot = [round(math.degrees(a), 2) for a in self.rot]
@@ -44,11 +53,9 @@ def projectPoint(point):
     global CAMERA_DEPTH
     global SCREEN_SIZE
 
-    # print(point)
-
     if point[2] > 0:
         x = (SCREEN_SIZE[0]/2)+point[0]*CAMERA_DEPTH/point[2]
-        y = (SCREEN_SIZE[1]/2)+point[1]*CAMERA_DEPTH/point[2]
+        y = (SCREEN_SIZE[1]/2)-point[1]*CAMERA_DEPTH/point[2]
     else:
         x = y = -999
 
@@ -60,17 +67,22 @@ def getDistance(point, cam):
     dist = math.sqrt((point[0])**2+(point[1])**2+(point[2])**2)
     return dist
 
-def fixTan(result, opp, adj):
+def fixTan(opp, adj):
+    if adj == 0:
+        if opp > 0:
+            return math.pi/2
+        elif opp < 0:
+            return -math.pi/2
+        else:
+            return 0
+    result = math.atan(opp/adj)
     if opp < 0:
         if adj < 0:
             # Third quadrant, result is positive
-            return math.pi - result
+            return result - math.pi
         elif adj > 0:
             # Second quadrant, result is negative
             return result
-        else:
-            # 180 degrees
-            return math.pi
     elif opp > 0:
         if adj > 0:
             # First quadrant, result is positive
@@ -78,36 +90,23 @@ def fixTan(result, opp, adj):
         elif adj < 0:
             # Fourth Quadrant, result is negative
             return math.pi+result
-    else:
-        if adj > 0:
-            # 0 degrees
-            return 0
-        elif adj < 0:
-            # 180 degrees
-            return math.pi
-        else:
-            return 0
+    elif adj < 0:
+        # 180 degrees
+        return math.pi
+    return 0
 
 def getLocalPos(point, camera):
     vecMag = getDistance(point, camera)
     point = [point[a]-camera.pos[a] for a in range(3)]
 
-    camThetas = list(camera.rot[:-1])
-
-    # Break out if the point has z of 0
-    if point[2] == 0:
-        return (-5, -5, -5)
-
     # Calculate the angles
-    # TODO Adjust the angles for the different quadrants
     # x/z
-    xtheta = fixTan(math.atan(point[0]/point[2]), point[0], point[2])
+    xtheta = fixTan(point[0], point[2])-camera.rot[0]
     # y/xzMag
-    ytheta = fixTan(math.atan(point[1]/math.sqrt(point[2]**2+point[0]**2)), point[1], math.sqrt(point[2]**2+point[0]**2))
+    xzMag = math.sqrt(point[2]**2+point[0]**2)
+    ytheta = fixTan(point[1], xzMag)-camera.rot[1]
 
-    xtheta, ytheta = [xtheta-camThetas[0], ytheta-camThetas[1]]
-
-    y = -vecMag*math.sin(ytheta)
+    y = vecMag*math.sin(ytheta)
 
     m = math.sqrt(vecMag**2-y**2)
     x = m*math.sin(xtheta)
@@ -123,8 +122,12 @@ if __name__ == "__main__":
     for z in range(-4, 5):
         for y in range(-4, 5):
             for x in range(-4, 5):
-                if x*z in [16, -16] or x*y in [16, -16] or z*y in [16, -16]:
+                # if abs(x*y*z)%16 == 0:
+                # if x*z in [16, -16] or x*y in [16, -16] or z*y in [16, -16]:
+                if abs(x) == 4 or abs(y) == 4 or abs(z) == 4:
                     points.append((x, y, z))
+
+    # points = [(a, 0, 5) for a in range(-20, 20)]
 
     clock = pygame.time.Clock()
     while True:
@@ -146,6 +149,8 @@ if __name__ == "__main__":
         elif keys[pygame.K_s]:
             cam.rot[1] -= 0.005
 
+        if pygame.time.get_ticks()%5000 < 10:
+            print('Average FPS:', cam.fps)
 
         pygame.display.flip()
     clock.tick(60)
