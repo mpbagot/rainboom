30bfb0e9c14b196f99c4b9466ec48107c6a271d7
Mitchell Bagot <packof14eggies@gmail.com>
2018-01-08 23:14:36 +1100

    Separate Math Function And Add Classes For Modularisation

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 import math
 from time import sleep, time
 
+from math_helper import fixTan
+
 #Initialise the constants
 SCREEN_SIZE = (1200, 675)
 # SCREEN_SIZE = (800, 450)
@@ -19,25 +21,41 @@ class Camera:
         self.rot = rot
         self.pos = pos
         self.fps = 0
+        self.screen = screen
+        self.scene = None
 
-    def renderPoints(self, points):
+    def setScene(self, scene):
+        '''
+        Set the scene to be rendered by this camera
+        '''
+        self.scene = scene
+
+    def renderScene(self):
+        '''
+        Render the scene that this camera is set to render
+        '''
         start = time()
-        # points2 = [getLocalPos(point, self) for point in points]
-        # print(points2)
-        for point in points:
-            localPos = getLocalPos(point, self)
-            if localPos[2] < 0:
-                continue
-            dist = getDistance(point, self)
-            scale = int((1-(dist/RENDER_DISTANCE))*10) if dist < RENDER_DISTANCE else 0
 
-            screenPos = projectPoint(localPos)
-            try:
-                if 0 < screenPos[0] < SCREEN_SIZE[0] and 0 < screenPos[1] < SCREEN_SIZE[1]:
-                    pygame.draw.circle(screen, (0, 0, 0), screenPos, scale)
-            except OverflowError:
-                print('Scale:', scale)
-                print('Screen Position:', [round(a, 2) for a in screenPos])
+        if self.scene is None:
+            raise ValueError('The scene has not been set for this camera!')
+
+        for group in self.scene.groups:
+            group.render(self)
+            # localPos = point.getLocalPos(self)
+            # if localPos[2] <= 0:
+            #     continue
+            # dist = point.getDistance(self)
+            # if dist > RENDER_DISTANCE:
+            #     continue
+            # scale = int((1-(dist/RENDER_DISTANCE))*10) if dist < RENDER_DISTANCE else 0
+            #
+            # screenPos = point.projectPoint(localPos)
+            # try:
+            #     if 0 < screenPos[0] < SCREEN_SIZE[0] and 0 < screenPos[1] < SCREEN_SIZE[1]:
+            #         pygame.draw.circle(screen, (0, 0, 0), screenPos, scale)
+            # except OverflowError:
+            #     print('Scale:', scale)
+            #     print('Screen Position:', [round(a, 2) for a in screenPos])
         self.fps = 1/(time()-start)
 
     def renderDebug(self):
@@ -49,112 +67,182 @@ class Camera:
         text = font.render("Position:"+str(pos), True, (0, 0, 255))
         screen.blit(text, [10, 30])
 
-def projectPoint(point):
-    global CAMERA_DEPTH
-    global SCREEN_SIZE
+class Scene:
+    def __init__(self):
+        self.groups = []
 
-    if point[2] > 0:
-        x = (SCREEN_SIZE[0]/2)+point[0]*CAMERA_DEPTH/point[2]
-        y = (SCREEN_SIZE[1]/2)-point[1]*CAMERA_DEPTH/point[2]
-    else:
-        x = y = -999
+    def addGroup(self, group):
+        if isinstance(group, Object):
+            self.addObject(group)
+            return
+        self.groups.append(group)
 
-    # print(x, y)
-    return (int(x), int(y))
+    def addObject(self, obj):
+        group = Group()
+        group.addObject(obj)
+        self.groups.append(group)
 
-def getDistance(point, cam):
-    point = [point[a]-cam.pos[a] for a in range(3)]
-    dist = math.sqrt((point[0])**2+(point[1])**2+(point[2])**2)
-    return dist
+class Object:
+    def __init__(self):
+        self.polygons = []
 
-def fixTan(opp, adj):
-    if adj == 0:
-        if opp > 0:
-            return math.pi/2
-        elif opp < 0:
-            return -math.pi/2
+    def addPolygon(self, poly):
+        self.polygons.append(poly)
+
+    def render(self, cam):
+        for poly in self.polygons:
+            poly.render(cam)
+
+class Group:
+    def __init__(self):
+        self.objects = []
+
+    def addObject(self, obj):
+        self.objects.append(obj)
+
+    def render(self, cam):
+        for obj in self.objects:
+            obj.render(cam)
+
+class Triangle:
+    def __init__(self, vertices):
+        self.vertices = vertices
+
+    def render(self, cam):
+        '''
+        Render this point to the given camera's screen
+        '''
+        screenPoints = [vertex.render(cam) for vertex in self.vertices]
+
+        for a in range(len(screenPoints)):
+            pygame.draw.line(cam.screen, (0, 0, 0), screenPoints[a], screenPoints[(a+1)%len(screenPoints)], 3)
+
+class Vertex:
+    def __init__(self, x, y=0, z=0):
+        if isinstance(x, list):
+            self.pos = x
         else:
-            return 0
-    result = math.atan(opp/adj)
-    if opp < 0:
-        if adj < 0:
-            # Third quadrant, result is positive
-            return result - math.pi
-        elif adj > 0:
-            # Second quadrant, result is negative
-            return result
-    elif opp > 0:
-        if adj > 0:
-            # First quadrant, result is positive
-            return result
-        elif adj < 0:
-            # Fourth Quadrant, result is negative
-            return math.pi+result
-    elif adj < 0:
-        # 180 degrees
-        return math.pi
-    return 0
+            self.pos = [x, y, z]
 
-def getLocalPos(point, camera):
-    vecMag = getDistance(point, camera)
-    point = [point[a]-camera.pos[a] for a in range(3)]
+    def render(self, cam):
+        '''
+        Render this point to the given camera's screen
+        '''
+        localPos = self.getLocalPos(cam)
+        if localPos[2] <= 0:
+            return
+        dist = self.getDistance(cam)
+        if dist > RENDER_DISTANCE:
+            return
+        scale = int((1-(dist/RENDER_DISTANCE))*10) if dist < RENDER_DISTANCE else 0
 
-    # Calculate the angles
-    # x/z
-    xtheta = fixTan(point[2], point[0])+camera.rot[0]
-    # y/xzMag
-    xzMag = math.sqrt(point[2]**2+point[0]**2)
+        screenPos = self.projectPoint(localPos)
+        try:
+            if 0 < screenPos[0] < SCREEN_SIZE[0] and 0 < screenPos[1] < SCREEN_SIZE[1]:
+                pygame.draw.circle(cam.screen, (0, 0, 0), screenPos, scale)
+        except OverflowError:
+            print('Scale:', scale)
+            print('Screen Position:', [round(a, 2) for a in screenPos])
 
-    x = xzMag*math.cos(xtheta)
-    point[2] = xzMag*math.sin(xtheta)
+        return screenPos
 
-    ytheta = fixTan(point[1], point[2])-camera.rot[1]
+    def projectPoint(self, localPos):
+        '''
+        Project a 3D point to the 2D screen space
+        '''
+        global CAMERA_DEPTH
+        global SCREEN_SIZE
 
-    zyMag = math.sqrt(point[2]**2+point[1]**2)
+        x = (SCREEN_SIZE[0]/2)+localPos[0]*CAMERA_DEPTH/localPos[2]
+        y = (SCREEN_SIZE[1]/2)-localPos[1]*CAMERA_DEPTH/localPos[2]
 
-    y = zyMag*math.sin(ytheta)
-    z = zyMag*math.cos(ytheta)
+        return (int(x), int(y))
 
-    return (x, y, z)
+    def getDistance(self, cam):
+        '''
+        Get the distance of this point from the given camera
+        '''
+        point = [self.pos[a]-cam.pos[a] for a in range(3)]
+        dist = math.sqrt((point[0])**2+(point[1])**2+(point[2])**2)
+        return dist
+
+    def getLocalPos(self, camera):
+        '''
+        Get a position for the point with respect to the camera
+        '''
+        point = [self.pos[a]-camera.pos[a] for a in range(3)]
+
+        # Calculate the angle
+        # x/z
+        xtheta = fixTan(point[2], point[0])+camera.rot[0]
+        # xzMag
+        xzMag = math.sqrt(point[2]**2+point[0]**2)
+
+        x = xzMag*math.cos(xtheta)
+        point[2] = xzMag*math.sin(xtheta)
+
+        # Calculate the other angle
+        ytheta = fixTan(point[1], point[2])-camera.rot[1]
+
+        zyMag = math.sqrt(point[2]**2+point[1]**2)
+
+        y = zyMag*math.sin(ytheta)
+        z = zyMag*math.cos(ytheta)
+
+        return (x, y, z)
 
 if __name__ == "__main__":
-    cam = Camera([0, 0, 0], [0, 0, 0])
-    points = [(30, 30, 30), (10, 10, 10), (5, 5, 5), (0, 0, 20)]
-    points = [(0, 0, 5), (5, 0, 5), (-5, 0, 5), (3, 0, 3), (-3, 0, 3), (0, 5, 5)]
+    scene = Scene()
+    obj = Object()
+
+    cams = [Camera([0, 0, 0], [0, 0, 0]), Camera([0, 5, 5], [-math.pi/2, -math.pi/2, 0])]
+
+    camIndex = False
+
     # Generate a cube of points
     points = []
     for z in range(-4, 5):
         for y in range(-4, 5):
             for x in range(-4, 5):
-                # if abs(x*y*z)%16 == 0:
-                # if x*z in [16, -16] or x*y in [16, -16] or z*y in [16, -16]:
                 if abs(x) == 4 or abs(y) == 4 or abs(z) == 4:
-                    points.append((x, y, z))
+                    points.append(Vertex(x, y, z))
 
-    # points = [(a, 0, 5) for a in range(-20, 20)]
+    points = [Vertex(a, 0, 5) for a in range(-20, 20)]
+
+    points = [Vertex(0, 5, 5), Vertex(5, -5, 5), Vertex(-5, -5, 5)]
+    faces = [Triangle(points)]
+
+    for face in faces:
+        obj.addPolygon(face)
+
+    scene.addObject(obj)
+
+    cams[0].setScene(scene)
+    cams[1].setScene(scene)
 
     clock = pygame.time.Clock()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                pass
+                if event.key == pygame.K_c:
+                    camIndex = not camIndex
 
         screen.fill((255, 255, 255))
-        cam.renderPoints(points)
-        cam.renderDebug()
+        cams[int(camIndex)].renderScene()
+        cams[int(camIndex)].renderDebug()
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_d]:
-            cam.rot[0] += 0.01
+            cams[int(camIndex)].rot[0] += 0.01
         elif keys[pygame.K_a]:
-            cam.rot[0] -= 0.01
+            cams[int(camIndex)].rot[0] -= 0.01
         if keys[pygame.K_w]:
-            cam.rot[1] += 0.01
+            cams[int(camIndex)].rot[1] += 0.01
         elif keys[pygame.K_s]:
-            cam.rot[1] -= 0.01
+            cams[int(camIndex)].rot[1] -= 0.01
 
-        if pygame.time.get_ticks()%5000 < 10:
-            print('Average FPS:', cam.fps)
+        if pygame.time.get_ticks()%5000 < 50:
+            print('Average FPS:', cams[int(camIndex)].fps)
 
         pygame.display.flip()
 
---
+++ math_helper.py
@@ -0,0 +1,26 @@
+import math
+
+def fixTan(opp, adj):
+    if adj == 0:
+        if opp > 0:
+            return math.pi/2
+        elif opp < 0:
+            return -math.pi/2
+        else:
+            return 0
+    result = math.atan(opp/adj)
+    if adj > 0:
+        # First quadrant, result is positive
+        # Second quadrant, result is negative
+        return result
+    else:
+        if opp < 0:
+            # Third quadrant, result is positive
+            return result - math.pi
+        elif opp == 0:
+            # 180 degrees
+            return math.pi
+        elif opp > 0:
+            # Fourth Quadrant, result is negative
+            return math.pi +result
+    return 0
